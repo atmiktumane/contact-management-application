3fe50d4759b7885531da7a391fc0ddade0de4724
atmiktumane <atmiktumane72@gmail.com>
2024-05-25 20:28:33 +0530

    created button for show and hide contacts in ContactList

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactCard from "./ContactCard";
 import { Link } from "react-router-dom";
 
 function ContactList(props) {
   // console.log(props);
+
+  const [show, setShow] = useState(true);
+
   const deleteContactHandler = (id) => {
     props.getContactId(id);
   };
@@ -23,14 +26,23 @@ function ContactList(props) {
         <div className="bg-white shadow-md m-6 rounded p-6  max-w-md w-full">
           <div className="flex justify-between mb-6 ">
             <h3 className="text-xl font-semibold mt-1">Contact List</h3>
-            <Link to="/add-contact">
-              <button className="bg-emerald-400 hover:bg-emerald-600 px-3 py-1 rounded-md ">
-                Add Contact
-              </button>
+            <Link
+              to="/add-contact"
+              className="bg-emerald-400 hover:bg-emerald-600 px-3 py-1 rounded-md"
+            >
+              Add Contact
             </Link>
+
+            <button
+              onClick={() => setShow(!show)}
+              className="bg-cyan-400 hover:bg-cyan-600 px-3 py-1 rounded-md"
+            >
+              {show ? "hide contacts" : "show contacts"}
+            </button>
           </div>
 
-          <ul>{renderContactList}</ul>
+          {/* <ul>{show && renderContactList}</ul> */}
+          <ul>{show ? renderContactList : ""}</ul>
         </div>
       </div>
     </>
