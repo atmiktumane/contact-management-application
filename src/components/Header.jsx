9d07b6a5bfdc6cc3832e3133830cbfba1cb63c10
atmiktumane <atmiktumane72@gmail.com>
2024-05-30 19:40:56 +0530

    updated Header with Navlink and Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export const Header = () => {
   return (
-    <header>
-      <div className="w-full h-24 bg-slate-800 flex items-center justify-center">
-        <h1 className="text-white text-3xl font-medium">
-          Contact Management App
-        </h1>
+    <header className="w-full h-20 bg-slate-700 flex justify-between p-3 text-white">
+      <Link to="/" className="flex items-center">
+        <img className="w-18 h-16" src={logo} alt="" />
+        <span className="text-3xl">Contact Management App</span>
+      </Link>
+
+      <div className="flex items-center text-xl">
+        <NavLink
+          to="/"
+          className="bg-cyan-400/75 hover:bg-cyan-600 px-3 py-1 rounded-md mx-3 transition duration-700 ease-in-out"
+        >
+          ContactList
+        </NavLink>
       </div>
     </header>
   );
