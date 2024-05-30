76734b554f17b79e489a372e08773cd6112cb799
atmiktumane <atmiktumane72@gmail.com>
2024-05-30 19:42:03 +0530

    added Footer to application

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="h-14 bg-gray-400 flex justify-center items-center">
+      <span>Made @ ContactApp 2024</span>
+    </footer>
+  );
 };
